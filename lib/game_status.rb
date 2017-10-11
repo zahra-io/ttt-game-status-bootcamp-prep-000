d91e7fa0a348fbb571c768b7a3a42709e26b3f33
Zahra Khan <zarssskhan@gmail.com>
2017-10-11 07:13:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,8 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 end
